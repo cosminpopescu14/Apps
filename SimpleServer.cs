17c5e7c0ed10642377d117973782bce62543d55f
Cosmin Popescu <cosmin.popescu93@gmail.com>
2018-08-26 07:27:17 +0300

    deleted comented code

--- SimpleServer.cs
+++ SimpleServer.cs
@@ -71,36 +71,7 @@ namespace TrialNetProgramming1
                         Console.ReadLine();
                     });
                     socketTherad.Start();
-                }
-          
-                /*space  for  data
-                byte[] data = new byte[8012];
-                int k = socket.Receive(data);
-
-                //working with received data
-                for (int i = 0; i < k; i++)
-                {
-                    // Console.WriteLine(Convert.ToChar(data[i]));
-                    byte [] dataToHash = ASCIIEncoding.ASCII.GetBytes(data[i].ToString());
-                    byte[] hashData = null;
-
-                    SHA1 sha1 = new SHA1CryptoServiceProvider();
-                    hashData = sha1.ComputeHash(dataToHash);
-                    
-                   StringBuilder sb = new StringBuilder();
-
-                    foreach (var item in hashData)
-                        sb.Append(item.ToString("x2"));
-                        byte[] hashedData = ASCIIEncoding.ASCII.GetBytes(sb.ToString());
-                        socket.Send(hashedData);//sending processed data to client  
-                }
-                
-                //send ack 
-                ASCIIEncoding asen = new ASCIIEncoding();
-                socket.Send(asen.GetBytes("Server received : "));
-                Console.WriteLine("Sent ack!\n");
-                socket.Send(data); */ 
-               
+                }      
             }
             catch (Exception ex)
             {
